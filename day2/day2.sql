2e2aff4c025e55f0481ab1f4d5ac5621b0509c8d
Priyanshu14188 <85730007+Priyanshu14188@users.noreply.github.com>
2025-09-09 20:07:32 +0530

    Update day2.sql

--- day2/day2.sql
+++ day2/day2.sql
@@ -1,1 +1,4 @@
-
+-- This query will select all data in sales where quantity is greater than 3 
+Select * from sales where quantity>3;
+-- This query will select all sales and order according to price filter that in decending order
+Select * from sales ORDER BY price DESC;
