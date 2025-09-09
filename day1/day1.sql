f41e44c86c2bda2cd89cff989d52ce0dd3a8c6b1
Priyanshu14188 <85730007+Priyanshu14188@users.noreply.github.com>
2025-09-09 14:10:24 +0530

    Update day1.sql

--- day1/day1.sql
+++ day1/day1.sql
@@ -1,1 +1,31 @@
+CREATE TABLE sales (
+    id INTEGER PRIMARY KEY,
+    product TEXT,
+    quantity INTEGER,
+    price INTEGER
+);
 
+INSERT INTO sales (product, quantity, price) VALUES
+('Laptop', 2, 50000),
+('Phone', 5, 20000),
+('Tablet', 3, 15000);
+
+INSERT INTO sales (product, quantity, price) VALUES
+('Monitor', 4, 10000),
+('Keyboard', 6, 2000),
+('Mouse', 10, 800),
+('Headphones', 3, 2500),
+('Printer', 2, 12000),
+('Camera', 1, 30000),
+('Speakers', 5, 4000),
+('Smartwatch', 7, 7000),
+('Charger', 15, 500),
+('Refrigerator', 1, 45000),
+('Washing Machine', 1, 38000),
+('Microwave', 2, 12000),
+('Desk', 3, 15000),
+('Chair', 5, 8000),
+('Projector', 2, 25000);
+
+
+SELECT * FROM sales;
